2e1bbbdcd197dc76d028271beff5fea80da2def8
NBCharro <nbcharro@gmail.com>
2021-09-14 12:39:43 +0200

    working on floating label

--- src/components/Atom.js
+++ src/components/Atom.js
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import classes from './Atom.module.css';
 
 const Atom = (props) => {
+    const [animacion, setAnimacion] = useState(false);
+    const animacionHandler = () => {
+        setAnimacion((prev) => !prev);
+    };
     return (
         <>
-            <div className={classes.container}>
+            <div className={classes.contenedor}>
+                <div
+                    className={`${classes.electronOrbita} ${
+                        animacion && classes.electronOrbitaAnimacionPausada
+                    }`}
+                >
+                    <div className={classes.estela} />
+                    <div
+                        className={classes.electron}
+                        onMouseEnter={animacionHandler}
+                        onMouseLeave={animacionHandler}
+                        direccion="Inicio"
+                    />
+                    <div
+                        className={classes.electron2}
+                        onMouseEnter={animacionHandler}
+                        onMouseLeave={animacionHandler}
+                        direccion="Contacto"
+                    />
+                </div>
+                <div className={classes.neutron} />
+                <div className={classes.proton} direccion="Proyectos" />
+                <div className={classes.neutron2} />
+                <div className={classes.proton2} direccion="Skills" />
+            </div>
+            {/* <div className={classes.container}>
                 <div className={classes.sun}></div>
                 <div className={`${classes.orbit} ${classes.earthOrbit}`}>
                     <div className={`${classes.globe} ${classes.earth}`}></div>
@@ -11,16 +41,7 @@ const Atom = (props) => {
                 <div className={`${classes.orbit} ${classes.earthOrbit2}`}>
                     <div className={`${classes.globe} ${classes.earth2}`}></div>
                 </div>
-            </div>
-            <div className={classes.contenedor}>
-                <div className={classes.proton}></div>
-                <div className={classes.proton2}></div>
-                <div className={classes.electronOrbita}>
-                    <div className={classes.estela} />
-                    <div className={classes.electron} />
-                    <div className={classes.electron2} />
-                </div>
-            </div>
+            </div> */}
         </>
     );
 };
