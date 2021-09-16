8e20f1c6245143dbda0ebcc9f0df45011484693c
NBCharro <nbcharro@gmail.com>
2021-09-16 18:58:56 +0200

    working on projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,9 @@ const Contact = (props) => {
     let formulario = {};
     if (props.idioma !== 'es') {
         formulario = {
+            titulo: 'Contact',
+            tituloArray: ['C', 'o', 'n', 't', 'a', 'c', 't'],
+            subtitulo: `If you wanna get in touch, talk to me about projects or just say hi, fill up the awesome form below or send an email and let's talk.`,
             nombre: `Your Name *`,
             email: `Your Email *`,
             telefono: `Your Phone *`,
@@ -15,6 +18,9 @@ const Contact = (props) => {
     }
     if (props.idioma === 'es') {
         formulario = {
+            titulo: 'Contacto',
+            tituloArray: ['C', 'o', 'n', 't', 'a', 'c', 't', 'o'],
+            subtitulo: `Si quieres ponerte en contacto conmigo, hablar de proyectos o simplemente saludar, rellena el impresionante formulario que aparece a continuación o envía un correo electrónico y hablemos.`,
             nombre: `Nombre *`,
             email: `Email *`,
             telefono: `Telefono *`,
@@ -26,14 +32,15 @@ const Contact = (props) => {
         <div className={classes.contacto}>
             <Social />
             <p className={classes.estiloTag}>{'<h2>'}</p>
-            <h2>Contacto</h2>
+            {/* <h2>{formulario.titulo}</h2> */}
+            <h2 className={classes.titulo}>
+                {formulario.tituloArray.map((letra) => (
+                    <span key={Math.random()}>{letra}</span>
+                ))}
+            </h2>
             <p className={classes.estiloTag}>{'</h2>'}</p>
             <p className={classes.estiloTag}>{'<p>'}</p>
-            <p>
-                If you wanna get in touch, talk to me about a project
-                collaboration or just say hi, fill up the awesome form below or
-                send an email to EMAIL and ~let's talk.
-            </p>
+            <p>{formulario.subtitulo}</p>
             <p className={classes.estiloTag}>{'</p>'}</p>
             <p className={classes.estiloTag}>{'<form>'}</p>
             <form action="#">
@@ -53,17 +60,17 @@ const Contact = (props) => {
                     placeholder={formulario.telefono}
                     name="phone"
                     type="number"
-                    min="6"
-                    max="6"
+                    size="6"
                     required
                 />
                 <textarea
                     placeholder={formulario.mensaje}
                     type="text"
                     name="message"
+                    rows="4"
                 />
                 <input
-                    className="button"
+                    className={classes.boton}
                     id="submit"
                     value={formulario.enviar}
                     type="submit"
