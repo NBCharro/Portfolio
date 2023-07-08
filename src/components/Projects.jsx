d70fcbdfd5dcae7aa24dd998568dfac721d30809
Nelson Blanco Charro <nbcharro@gmail.com>
2023-07-08 09:46:56 +0200

    fixed link button php

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -99,9 +99,9 @@ function Projects() {
 							</p>
 						</div>
 						<div className="flex flex-nowrap justify-around lg:flex-wrap pb-4 lg:pb-2">
-							<a href="" disabled target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-primary text-black rounded-lg hover:bg-secondary hover:text-white border-2 border-primary hover:border-white lg:my-2">
+							<button disabled target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-400 rounded-lg lg:my-2 bg-gray-700 border-2 border-gray-400">
 								Visitar pagina
-							</a>
+							</button>
 							<a href="https://github.com/NBCharro/barberiaLaravel" target="_blank" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-primary text-black rounded-lg hover:bg-secondary hover:text-white border-2 border-primary hover:border-white lg:my-2">
 								Enlace a Github
 							</a>
