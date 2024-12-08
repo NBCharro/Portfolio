cca8873f635754c07f23d2cc790f704523df8617
Nelson Blanco Charro <nbcharro@gmail.com>
2024-12-08 16:01:00 +0100

    Update Link & CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,9 +24,6 @@ function Contact() {
 				<a href="https://www.linkedin.com/in/nbcharro" target="_blank" title="LinkedIn">
 					<img className="h-8" src="/images/social/linkedin.png" alt="Icono de Linkedin" />
 				</a>
-				<a href="https://twitter.com/nbcharro" target="_blank" title="Twitter" >
-					<img className="h-8" src="/images/social/twitter.png" alt="Icono de Twitter" />
-				</a>
 				<a href="https://wa.me/34614243007" target="_blank" title="Whatsapp">
 					<img className="h-8" src="/images/social/whatsapp.png" alt="Icono de Whatsapp" />
 				</a>
